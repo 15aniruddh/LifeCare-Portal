11607facfccc7d295c377c7a82e0dbdad920fa52
Aniruddh <aniruddh1995@yahoo.com>
2025-05-17 12:08:44 +0530

    Updated to react libraries and changed to yarn from npm

--- LifeCare-FrontEnd/src/index.js
+++ LifeCare-FrontEnd/src/index.js
@@ -1,23 +1,21 @@
-import { React } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
-import Footer from "./components/common/Footer";
-import Header from "./components/common/Header";
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
 
+// Create a root and render your app
+const root = createRoot(document.getElementById('root'));
 
-ReactDOM.render(
+root.render(
   <BrowserRouter>
-
-    <Header />    
+    <Header />
     <App />
     <Footer />
-
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
